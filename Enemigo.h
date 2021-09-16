3bc9e9126820debe2c9e1d17975e116723c74dbb
HD-VG <vargashectordanielgonzales@gmail.com>
2021-09-15 23:03:57 -0400

    agregando nuevas clases

--- Enemigo.h
+++ Enemigo.h
@@ -1,4 +1,16 @@
 #pragma once
 class Enemigo {
+private:
+	int anchoavatarenemigo;
+	int altoavatarenemigo;
 
+public:
+	int getAnchoAvatarEnemigo() { return anchoavatarenemigo; }
+	void setAnchoAvatar(int _anchoavatarenemigo) { anchoavatarenemigo = _anchoavatarenemigo; }
+
+	int getAltoAvatar() { return altoavatarenemigo; }
+	void setAltoAvatar(int _altoavatarenemigo) { altoavatarenemigo = _altoavatarenemigo; }
+
+	void Morirenemigo();
+	void Correrenemigo();
 };
